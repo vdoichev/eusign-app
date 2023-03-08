9f3247838efade08aa28fee37db649ded1b88b72
0678 <viacheslav.doichev@uspa.com.ua>
2023-03-08 13:39:51 +0200

    Add initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import {EndUser, EndUserConstants} from "euscp";
-import {error} from "@angular/compiler-cli/src/transformers/util";
+import {Component} from '@angular/core';
+import {EndUser, EndUserConstants, EndUserLibraryInfoSW} from "euscp";
+import {EndUserLibraryType} from "euscp/EndUserConstants";
 
 @Component({
   selector: 'app-root',
@@ -27,43 +27,137 @@ export class AppComponent {
     ]
   };
 
-  endUser = new EndUser('', EndUserConstants.EndUserLibraryType.SW);
+  // Бібліотека для роботи з файловими ключами, що не потребує
+// встановлення додатково ПЗ
+  private euSignFile = new EndUser(
+    undefined,
+    EndUserConstants.EndUserLibraryType.JS);
 
-  private initialize(): boolean {
-    console.log("Ініціалізація бібліотек");
-    this.endUser.GetLibraryInfo().
-    then(result => {
-      if (!result.supported) {
-        console.log("Бібліотека web-підпису не підтримується " +
-          "в вашому браузері або ОС");
-      }
+// Бібліотека для роботи з аппаратними носіями, що потребує
+// встановлення додатково ПЗ бібліотек web-підпису, web-розширення для браузера
+  private euSignKeyMedia = new EndUser(
+    undefined,
+    EndUserConstants.EndUserLibraryType.SW);
+  private keyMedias = [];
 
-      if (!result.loaded) {
-        // Бібліотека встановлена, але потребує оновлення
-        console.log("Бібліотека встановлена, але потребує оновлення");
-      }
-      console.log("this.endUser.IsInitialized()");
-      return this.endUser.IsInitialized();
-    }).
-    then(result =>  {
-        if (result) {
-          console.log("EndUser: already initialized");
-          return;
-        }
-
-        console.log("EndUser: initializing...");
-        return this.endUser.Initialize(this.euSettings);
-      }).
-    catch(error=>{
-      console.log(error);
-      return false;
-    });
-    console.log("Бібліотека ще не ініціалізована");
-    return false;
-  }
+  public euSign = this.euSignKeyMedia;
 
   ngOnInit() {
     console.log("ngOnInit головного компонента");
-    this.initialize();
+    this.setLibraryType(EndUserConstants.EndUserLibraryType.SW);
+
   }
+
+  setLibraryType(type: EndUserLibraryType) {
+    console.log("Встановлення типу бібліотеки" + type);
+    switch (type) {
+      case EndUserConstants.EndUserLibraryType.JS:
+        this.euSign = this.euSignFile;
+        break;
+      case EndUserConstants.EndUserLibraryType.SW:
+        this.euSign = this.euSignKeyMedia;
+        break;
+    }
+
+    this.initialize()
+      .then(result => {
+        if (this.euSign == this.euSignFile)
+          return [];
+
+        return this.euSign.GetKeyMedias();
+      })
+      .then(result => {
+        // setKeyMedias(keyMedias);
+        //
+        // signBlock.style.display = 'block';
+      })
+      .catch(e => {
+        let msg = (e.message || e);
+
+        console.log("Initialize error: " + msg);
+
+        alert('Виникла помилка при ініціалізації бібліотеки. ' +
+          'Опис помилки: ' + msg);
+      })
+
+  }
+
+  private initialize(): Promise<void> {
+    return new Promise((resolve) => {
+      if (this.euSign == this.euSignFile) {
+        this.euSign.IsInitialized()
+          .then((result) => {
+            if (result) {
+              console.log("EndUser: already initialized");
+              resolve();
+              return;
+            }
+
+            console.log("EndUser: initializing...");
+            return this.euSign.Initialize(this.euSettings)
+          })
+          .then((result) => {
+            console.log("EndUser: initialized");
+            resolve()
+          })
+          .catch(error => {
+            console.log("EndUser: error"+error);
+            // reject();
+          });
+      } else {
+        // Перевірка чи встановлені необхідні модулі для роботи криптографічної бібліотеки
+        this.euSign.GetLibraryInfo()
+          .then(result => {
+            if (!result.supported) {
+              throw "Бібліотека web-підпису не підтримується " +
+              "в вашому браузері або ОС";
+            }
+
+            if (!result.loaded && result instanceof EndUserLibraryInfoSW) {
+              // Бібліотека встановлена, але потребує оновлення
+              if (result.isNativeLibraryNeedUpdate) {
+                throw "Бібліотека web-підпису потребує оновлення. " +
+                "Будь ласка, встановіть оновлення за посиланням " +
+                result.nativeLibraryInstallURL;
+              }
+
+              // Якщо браузер підтримує web-розширення рекомендується
+              // додатково до нативних модулів встановлювати web-розширення
+              // Увага! Встановлення web-розширень ОБОВ'ЯЗКОВЕ для ОС Linux та ОС Windows Server
+              if (result.isWebExtensionSupported &&
+                !result.isWebExtensionInstalled) {
+                throw "Бібліотека web-підпису потребує встановлення web-розширення. " +
+                "Будь ласка, встановіть web-розширення за посиланням " +
+                result.webExtensionInstallURL + " та оновіть сторінку";
+              }
+
+              // Бібліотека (нативні модулі) не встановлені
+              throw "Бібліотека web-підпису потребує встановлення. " +
+              "Будь ласка, встановіть бібліотеку за посиланням " +
+              result.nativeLibraryInstallURL + " та оновіть сторінку";
+            }
+
+            return this.euSign.IsInitialized();
+          })
+          .then((result) => {
+            if (result) {
+              console.log("EndUser: already initialized");
+              resolve();
+              return;
+            }
+
+            console.log("EndUser: initializing...");
+            return this.euSign.Initialize(this.euSettings)
+          }).then((result) => {
+          console.log("EndUser: initialized");
+          resolve()
+        })
+          .catch(error => {
+            console.log("EndUser: error"+error);
+          });
+      }
+    })
+  }
+
+
 }
